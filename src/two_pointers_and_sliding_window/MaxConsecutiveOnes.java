011ea8b3012c93404f4cd45e7fbe98a9731b7591
vamshi gudimalla <vamshigudimalla38@gmail.com>
2025-02-21 23:57:20 +0530

    MaxConsectiveOnes better and optimal solutions

--- src/two_pointers_and_sliding_window/MaxConsecutiveOnes.java
+++ src/two_pointers_and_sliding_window/MaxConsecutiveOnes.java
@@ -29,11 +29,72 @@ public class MaxConsecutiveOnes {
 	return maxLen;
     }
 
+    public static int better(int arr[], int k) {
+	int zeroCount = 0;
+	int maxLen = Integer.MIN_VALUE;
+	int len = 0;
+	int n = arr.length;
+
+	int l = 0;
+	int r = 0;
+
+	while (r < n) {
+	    if (arr[r] == 0)
+		zeroCount++;
+
+	    while (zeroCount > k) {
+		if (arr[l] == 0)
+		    zeroCount--;
+		l++;
+	    }
+
+	    if (zeroCount <= k) {
+		len = r - l + 1;
+		maxLen = Math.max(maxLen, len);
+	    }
+
+	    r++;
+	}
+
+	return maxLen;
+    }
+
+    public static int optimal(int arr[], int k) {
+	int zeroCount = 0;
+	int maxLen = Integer.MIN_VALUE;
+	int len = 0;
+	int n = arr.length;
+
+	int l = 0;
+	int r = 0;
+
+	while (r < n) {
+	    if (arr[r] == 0)
+		zeroCount++;
+
+	    if (zeroCount > k) {
+		if (arr[l] == 0)
+		    zeroCount--;
+		l++;
+	    }
+
+	    if (zeroCount <= k) {
+		len = r - l + 1;
+		maxLen = Math.max(maxLen, len);
+	    }
+
+	    r++;
+	}
+
+	return maxLen;
+
+    }
+
     public static void main(String[] args) {
 
-	int arr[] = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 };
+	int arr[] = { 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1 };
 
-	int res = bruteForce(arr, 2);
+	int res = optimal(arr, 2);
 
 	System.out.print("maxOnes = " + res);
 
