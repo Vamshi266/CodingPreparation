ed3c5c1c236fa036733ceb54ce0fd19b0c7412e8
vamshi gudimalla <vamshigudimalla38@gmail.com>
2025-05-21 17:38:36 +0530

    solved 1.Diameter of Binary Tree 2.maxWidth of a Binary tree

--- src/trees/DiameterOfaBinaryTree.java
+++ src/trees/DiameterOfaBinaryTree.java
@@ -2,16 +2,37 @@ package trees;
 
 public class DiameterOfaBinaryTree {
 
-    static int diameter;
+    /*
+     * 
+     * Diameter = longest path btw two nodes (no of edges btw them)
+     * 
+     */
 
-    public static int findDiameter(Node root) {
+    // Time Complexity = O(N^2)
+    // Space Complexity = O(logN)
+    public static void findDiameter1(Node root, int[] max) {
+	if (root == null)
+	    return;
+
+	int lh = MaxDepthOfBinaryTree.maxDepth(root.left);
+	int rh = MaxDepthOfBinaryTree.maxDepth(root.right);
+
+	max[0] = Math.max(max[0], lh + rh);
+
+	findDiameter1(root.left, max);
+	findDiameter1(root.right, max);
+    }
+
+    // Time Complexity = O(N)
+    // Space Complexity = O(logN)
+    public static int findDiameter2(Node root, int[] max) {
 	if (root == null)
 	    return 0;
 
-	int lh = findDiameter(root.left);
-	int rh = findDiameter(root.right);
+	int lh = findDiameter2(root.left, max);
+	int rh = findDiameter2(root.right, max);
 
-	diameter = Math.max(diameter, lh + rh);
+	max[0] = Math.max(max[0], lh + rh);
 
 	return 1 + Math.max(lh, rh);
 
@@ -32,9 +53,11 @@ public class DiameterOfaBinaryTree {
 	root.right.right.right = new Node(8);
 	root.right.right.right.right = new Node(9);
 
-	findDiameter(root);
+	int max[] = new int[1];
 
-	System.out.println("diameter of the tree = " + diameter);
+	findDiameter2(root, max);
+
+	System.out.println("diameter of the tree = " + max[0]);
 
     }
 
